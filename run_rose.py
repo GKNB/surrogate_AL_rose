ee28fbd8e40aac685b910c28e44f277469a60b16
GKNB <tw2507@columbia.edu>
2025-07-17 02:52:26 -0700

    Add a jupyter notebook and fix bug

--- run_rose.py
+++ run_rose.py
@@ -17,8 +17,8 @@ code_path = f'{sys.executable} {os.getcwd()}'
 
 #FIXME!! I am utility task!!
 @learner.simulation_task
-def bootstrap(*args, pipeline_dir):
-    return Task(executable=f'{code_path}/bootstrap.py --pipeline_dir {pipeline_dir}')
+def bootstrap(*args, pipeline_dir, input_data_dir):
+    return Task(executable=f'{code_path}/bootstrap.py --pipeline_dir {pipeline_dir} --input_data_dir {input_data_dir}')
     
 #FIXME!! For this two tasks, need to specify if using GPU, and other variables!!
 @learner.training_task
@@ -26,29 +26,33 @@ def training(*args, model, config_file, iteration, pipeline_dir):
     return Task(executable=f'{code_path}/train.py --model {model} --config {config_file} --iteration {iteration} --pipeline_dir {pipeline_dir}')
 
 @learner.active_learn_task
-def active_learning(*args, model, config_file, iteration, pipeline_dir):
-    return Task(executable=f'{code_path}/active.py --model {model} --config {config_file} --iteration {iteration} --pipeline_dir {pipeline_dir}')
+def active_learning(*args, model, config_file, iteration, pipeline_dir, num_new_samples):
+    return Task(executable=f'{code_path}/active.py --model {model} --config {config_file} --iteration {iteration} --pipeline_dir {pipeline_dir} --n_new_samples {num_new_samples}')
 
-def teach_single_pipeline(model, config_file, pipeline_dir, num_iter):
-    iter_id = 0
+def teach_single_pipeline(model, input_data_dir, config_file, pipeline_dir, num_iter, num_new_samples):
+    iter_id = 1
     print("Start doing bootstrap (only once!)")
-    bs = bootstrap(pipeline_dir=pipeline_dir)
+    bs = bootstrap(pipeline_dir=pipeline_dir, input_data_dir=input_data_dir)
     bs.result()
-    while iter_id < num_iter - 1:   # n iter means in total n traning and n-1 al
+    while iter_id < num_iter:   # n iter means in total n traning and n-1 al
         print(f"Start doing iteration {iter_id}")
         train = training(model=model, config_file=config_file, iteration=iter_id, pipeline_dir=pipeline_dir)
-        active_learn = active_learning(train, model=model, config_file=config_file, iteration=iter_id, pipeline_dir=pipeline_dir)
-        active.result()
+        if iter_id == num_iter - 1:
+            train.result()
+        else:
+            active_learn = active_learning(train, model=model, config_file=config_file, iteration=iter_id, pipeline_dir=pipeline_dir, num_new_samples=num_new_samples)
+            active_learn.result()
         iter_id += 1
 
 def teach():
     submitted_pipelines = []
-    # Some conf list we set, like seed
-    conf_list = []
     async_pipeline = learner.as_async(teach_single_pipeline)
-    for model in ["bnn", "gpr"]:
-        for config_file in conf_list:
-            submitted_pipelines.append(async_pipeline(model=model, config_file=config_file, pipeline_dir=pipeline_dir, num_iter=10))
+    model = "gpr"
+    input_data_dir = "/pscratch/sd/u/usatlas/globus-compute-test/Tianle_test/nano-confinement/surrogate_AL/data/"
+    conf_list = ["/pscratch/sd/u/usatlas/globus-compute-test/Tianle_test/nano-confinement/rose_exp_1/config/gpr.yaml", "/pscratch/sd/u/usatlas/globus-compute-test/Tianle_test/nano-confinement/rose_exp_1/config/gpr_01.yaml"]
+    pipeline_dir_list = ["/pscratch/sd/u/usatlas/globus-compute-test/Tianle_test/nano-confinement/rose_exp_1/experiment/test_04", "/pscratch/sd/u/usatlas/globus-compute-test/Tianle_test/nano-confinement/rose_exp_1/experiment/test_05"]
+    for config_file, pipeline_dir in zip(conf_list, pipeline_dir_list):
+        submitted_pipelines.append(async_pipeline(model=model, input_data_dir=input_data_dir, config_file=config_file, pipeline_dir=pipeline_dir, num_iter=10, num_new_samples=10))
     [p.result() for p in submitted_pipelines]
 
 teach()
